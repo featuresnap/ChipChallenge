f5b22101d35484075ab1abc1d8aa5a042e824360
Patrick Kelly <patrick.kelly@sherwin.com>
2017-10-25 06:20:36 -0400

    Solution finding API takes shape

--- src/CodeChallenge/ChipComputer.cs
+++ src/CodeChallenge/ChipComputer.cs
@@ -1,14 +1,46 @@
-﻿using Chip=System.ValueTuple<string,string>;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Chip = System.ValueTuple<string, string>;
 
 namespace CodeChallenge
 {
     public class ChipComputer
     {
+        public const string NO_RESULT = "No result found";
+
         private PermutationStrategy<Chip> _permutationStrategy;
+        private string DUMMY = "DUMMY";
 
         public ChipComputer(PermutationStrategy<Chip> permutationStrategy)
         {
             _permutationStrategy = permutationStrategy;
+        }
+
+        public IEnumerable<string> Check(string startColor, string endColor, IEnumerable<Chip> chips)
+        {
+            bool isMatch(IEnumerable<Chip> trialSequence) => Match(startColor, endColor, trialSequence);
+
+            var trialSequences = _permutationStrategy.Permute(chips);
+            var firstSolution = trialSequences.FirstOrDefault(isMatch);
+
+            if (firstSolution != null)
+            {
+                return firstSolution.Select(x=> x.Item1 + ", " + x.Item2);
+            }
+
+            return new[] {NO_RESULT};
+
+        }
+
+        private bool Match(string startColor, string endColor, IEnumerable<Chip> chips)
+        {
+            bool localMatch(Chip first, Chip next) => first.Item2 == next.Item1;
+
+            var startChip = (DUMMY, startColor);
+            var endChip = (endColor, DUMMY);
+            var pairMatchResults = chips.Prepend(startChip).Zip(chips.Append(endChip), localMatch);
+            return pairMatchResults.All(x => x == true);
         }
     }
 }
--- test/CodeChallengeTests/ChipComputerShould.cs
+++ test/CodeChallengeTests/ChipComputerShould.cs
@@ -1,20 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
 using Xunit;
 using CodeChallenge;
+using Chip = System.ValueTuple<string, string>;
 
 namespace CodeChallengeTests
 {
     public class ChipComputerShould
     {
-        [Fact]
-        public void ReportFailureWhenNoSolutions()
-        {
 
+        [Fact]
+        public void ReportNoResultWhenNoResultFound()
+        {
+            var permutation = new FakePermutationStrategy(_ => new List<IEnumerable<Chip>> { new Chip[] { } });
+
+            var computer = new ChipComputer(permutation);
+            var result = computer.Check(
+                "White",
+                "Yellow",
+                new[] { ("Red", "Green"), ("Blue", "Purple") });
+
+            Assert.Collection(result, firstLine => Assert.Equal(ChipComputer.NO_RESULT, firstLine));
+        }
+
+        [Fact]
+        public void ReportMatch()
+        {
+            var permutation = new FakePermutationStrategy(_ => new List<IEnumerable<Chip>> { new[] { ("Green", "Blue") } });
+
+            var computer = new ChipComputer(permutation);
+            var result = computer.Check(
+                "Green",
+                "Blue",
+                new[] { ("Green", "Blue")});
+
+            Assert.Collection(result, firstLine => Assert.Equal("Green, Blue", firstLine));
         }
 
 
-        
+
+    }
+
+    internal class FakePermutationStrategy : PermutationStrategy<Chip>
+    {
+        private Func<IEnumerable<(string, string)>, IEnumerable<IEnumerable<(string, string)>>> _fakeBehavior;
+
+        public FakePermutationStrategy(Func<IEnumerable<Chip>, IEnumerable<IEnumerable<Chip>>> fakeBehavior)
+        {
+            _fakeBehavior = fakeBehavior;
+        }
+
+        public IEnumerable<IEnumerable<Chip>> Permute(IEnumerable<Chip> input)
+        {
+            return _fakeBehavior(input);
+        }
     }
 
 }
